3031432a804458b7d5636211a53299c09fde1caa
nibudon <729686144@qq.com>
2019-06-10 12:36:46 +0800

    test

---
+++ src/main/java/com/nibudon/TestsshApplication.java
@@ -0,0 +1,13 @@
+package com.nibudon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestsshApplication.class, args);
+    }
+
+}
